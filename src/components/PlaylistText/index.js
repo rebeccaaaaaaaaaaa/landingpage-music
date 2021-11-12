ce81c486da74e0edff8a2f6c34bedb5e272dc5ce
rebeccaaaaaaaaaaa <rebeccaignacio@outlook.com>
2021-11-12 20:51:19 -0300

    link redes sociais

--- src/components/PlaylistText/index.js
+++ src/components/PlaylistText/index.js
@@ -5,7 +5,7 @@ const PlaylistText = () => {
         <div className={style.playlistText}>
             <h1 className={style.title__playlistText}> playlist </h1>
             <p className={style.paragraph__playlistText}> as melhores</p>
-            <a href="/" target="_blank" rel="noopener noreferrer"className={style.button_playlistText}> ouça agora </a>
+            <a href="https://open.spotify.com/artist/1yR65psqiazQpeM79CcGh8" target="_blank" rel="noopener noreferrer"className={style.button_playlistText}> ouça agora </a>
         </div>
     );
 }
--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -3,18 +3,17 @@ import FacebookIcon from '../../assets/facebook.png'
 import InstagramIcon from '../../assets/instagram.png'
 import TwitterIcon from '../../assets/twitter.png'
 import YoutubeIcon from '../../assets/youtube.png'
-import SoundCloudIcon from '../../assets/soundcloud.png'
+
 
 
 const Social = () => {
     return (
         <div className={style.social}>
           <ul className={style.social__ul}>
-              <li> <a href="/" rel="nofollow noopener noreferrer" target="_blank"> <img src={FacebookIcon} alt="Logo FacebookIcon"/></a> </li>
-              <li> <a href="/" rel="nofollow noopener noreferrer"> <img src={InstagramIcon} alt="Logo InstagramIcon"/></a> </li>
-              <li> <a href="/" rel="nofollow noopener noreferrer"><img src={TwitterIcon} alt="Logo TwitterIcon"/></a></li>
-              <li> <a href="/" rel="nofollow noopener noreferrer"><img src={YoutubeIcon} alt="Logo YoutubeIcon"/></a> </li>
-              <li> <a href="/" rel="nofollow noopener noreferrer"><img src={SoundCloudIcon} alt="Logo SoundCloudIcon"/> </a></li>
+              <li> <a href="https://www.facebook.com/mariliamendoncaoficial" rel="nofollow noopener noreferrer" target="_blank"> <img src={FacebookIcon} alt="Logo FacebookIcon"/></a> </li>
+              <li> <a href="https://www.instagram.com/mariliamendoncacantora/" rel="nofollow noopener noreferrer"> <img src={InstagramIcon} alt="Logo InstagramIcon"/></a> </li>
+              <li> <a href="https://twitter.com/MariliaMReal" rel="nofollow noopener noreferrer"><img src={TwitterIcon} alt="Logo TwitterIcon"/></a></li>
+              <li> <a href="https://www.youtube.com/channel/UCwfEOn0O1DWcyTgzVVu28ig" rel="nofollow noopener noreferrer"><img src={YoutubeIcon} alt="Logo YoutubeIcon"/></a> </li>
           </ul>
         </div>
     );
